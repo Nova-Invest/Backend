607278e9db6695db4d297f759992dc089b6c40ae
believe <believeosawaru2@gmail.com>
2025-03-27 15:14:52 +0100

    update

--- controllers/paymentController.js
+++ controllers/paymentController.js
@@ -1,5 +1,5 @@
-const axios = require('axios');
-const User = require('../models/User');
+const axios = require("axios");
+const User = require("../models/User");
 const paystackSecretKey = process.env.PAYSTACK_SECRET_KEY;
 
 /**
@@ -10,7 +10,7 @@ const verifyPayment = async (req, res) => {
   const { reference } = req.query;
 
   if (!reference) {
-    return res.status(400).json({ message: 'No reference provided' });
+    return res.status(400).json({ message: "No reference provided" });
   }
 
   try {
@@ -26,11 +26,11 @@ const verifyPayment = async (req, res) => {
 
     const { status, amount, metadata } = response.data.data;
 
-    if (status === 'success') {
+    if (status === "success") {
       // Find the user using the userId from the metadata
       const user = await User.findById(metadata.userId);
       if (!user) {
-        return res.status(404).json({ message: 'User not found' });
+        return res.status(404).json({ message: "User not found" });
       }
 
       // Convert amount from kobo to Naira
@@ -41,25 +41,81 @@ const verifyPayment = async (req, res) => {
 
       // Add the transaction to the user's transactions array
       user.transactions.push({
-        type: 'fund_wallet',
+        type: "fund_wallet",
         amount: amountInNaira,
         date: new Date(),
-        status: 'completed',
+        status: "completed",
       });
 
       // Save the updated user document
       await user.save();
 
       // Return success response
-      res.status(200).json({ message: 'Payment successful', user });
+      res.status(200).json({ message: "Payment successful", user });
     } else {
       // Payment was not successful
-      res.status(400).json({ message: 'Payment not successful' });
+      res.status(400).json({ message: "Payment not successful" });
     }
   } catch (error) {
-    console.error('Error verifying Paystack payment:', error);
-    res.status(500).json({ message: 'Error verifying payment' });
+    console.error("Error verifying Paystack payment:", error);
+    res.status(500).json({ message: "Error verifying payment" });
   }
 };
 
-module.exports = { verifyPayment };
+// Withdrawal
+const createRecipient = async (req, res) => {
+  try {
+    const { account_number, bank_code, name } = req.body;
+
+    const response = await axios.post(
+      "https://api.paystack.co/transferrecipient",
+      {
+        type: "nuban",
+        name,
+        account_number,
+        bank_code,
+        currency: "NGN",
+      },
+      {
+        headers: {
+          Authorization: `Bearer ${paystackSecretKey}`,
+          "Content-Type": "application/json",
+        },
+      }
+    );
+
+    res.json(response.data);
+  } catch (error) {
+    console.error("Error handling withdrawal:", error);
+    res.status(500).json({ message: "Error verifying payment" });
+  }
+};
+
+const withdraw = async (req, res) => {
+  try {
+    const { amount, recipient_code } = req.body;
+
+    const response = await axios.post(
+      "https://api.paystack.co/transfer",
+      {
+        source: "balance",
+        amount,
+        recipient: recipient_code,
+        reason: "Test Withdrawal",
+      },
+      {
+        headers: {
+          Authorization: `Bearer ${paystackSecretKey}`,
+          "Content-Type": "application/json",
+        },
+      }
+    );
+
+    res.json(response.data);
+  } catch (error) {
+    console.error("Error handling withdrawal:", error);
+    res.status(500).json({ message: "Error verifying payment" });
+  }
+};
+
+module.exports = { verifyPayment, createRecipient, withdraw };
--- routes/paymentRoutes.js
+++ routes/paymentRoutes.js
@@ -1,9 +1,15 @@
-const express = require('express');
-const { verifyPayment } = require('../controllers/paymentController');
+const express = require("express");
+const {
+  verifyPayment,
+  createRecipient,
+  withdraw,
+} = require("../controllers/paymentController");
 
 const router = express.Router();
 
 // Verify payment
-router.get('/verify', verifyPayment);
+router.get("/verify", verifyPayment);
+router.post("/create-recipient", createRecipient);
+router.post("/withdraw", withdraw);
 
 module.exports = router;
